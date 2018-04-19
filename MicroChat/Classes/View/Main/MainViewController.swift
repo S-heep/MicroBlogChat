cf10f9c2e96713bea69fd81800279403085d422a
S-heep <991207691@qq.com>
2018-04-19 08:42:53 +0800

    【微博】搭建项目框架

---
+++ MicroChat/Classes/View/Main/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  MicroChat
+//
+//  Created by 王娜 on 2018/4/18.
+//  Copyright © 2018年 王娜. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+// MARK: - 设置界面
+
+extension MainViewController {
+
+    /// 添加 
+    private func addChildViewController() {
+
+    }
+}
